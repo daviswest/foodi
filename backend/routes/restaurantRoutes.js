dab56c634da49b5bca77b068cdf0aa09e2ba1d1b
daviswest <djwest06@louisville.edu>
2025-04-17 14:10:08 -0400

    details route

--- backend/routes/restaurantRoutes.js
+++ backend/routes/restaurantRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const restaurantController = require('../controllers/restaurantController');
 
 router.post('/find-restaurants', restaurantController.findRestaurants);
+router.get("/details", restaurantController.getRestaurantDetails);
 
 module.exports = router;
