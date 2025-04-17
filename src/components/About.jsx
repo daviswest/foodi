59c84cb255f1f69ac78c6a85f0ec52414286040a
daviswest <djwest06@louisville.edu>
2025-04-17 14:17:21 -0400

    style and formatting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,9 @@ import { React } from 'react'
 
 const About = () => {
   return(
-    <h1>A lil bit about the proj</h1>
+    <div style={{display: 'flex', flex: 1,}}>
+      <h1>A lil bit about the proj</h1>
+    </div>
   );
 };
 
