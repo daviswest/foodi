42cb1c63f2e4e90636ff35995d56376c8412484e
daviswest <djwest06@louisville.edu>
2025-04-17 14:09:20 -0400

    favorites provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import './index.css';
 import App from './App.jsx';
 import { LocationProvider } from './contexts/LocationContext';
 import { AuthProvider } from './contexts/AuthContext.jsx';
-
+import { FavoritesProvider } from './contexts/FavoritesContext.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+      <FavoritesProvider>
       <LocationProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </LocationProvider>
+      </FavoritesProvider>
     </AuthProvider>
   </StrictMode>,
 );
