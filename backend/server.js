d981b743445723bc29639f425f09dd7e3132b924
daviswest <djwest06@louisville.edu>
2025-05-14 11:10:53 -0400

    frontend url allowed in cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://reliable-toffee-506b30.netlify.app/',
   credentials: true,
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
